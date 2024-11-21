95c7a76102e03ab4b98d31bedd400d20e741f8ec
Adam Devlin <145337473+G00437074@users.noreply.github.com>
2024-11-21 16:01:01 +0000

    Adding Instance Variables

--- src/ie/atu/songmanager/Song.java
+++ src/ie/atu/songmanager/Song.java
@@ -2,4 +2,12 @@ package ie.atu.songmanager;
 
 public class Song {
 
+    // Instance Variables 
+    private String SongName; // Unique ID
+    private int NumberofPlays; 
+    private double Length;
+    private boolean isSold;
+
+
+
 }
