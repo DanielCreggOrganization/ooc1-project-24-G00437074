83340c879f556a614d7d55bb2045f1bdce0a152d
Adam Devlin <145337473+G00437074@users.noreply.github.com>
2024-11-21 16:16:37 +0000

    Getters and Setters

--- src/ie/atu/songmanager/Song.java
+++ src/ie/atu/songmanager/Song.java
@@ -3,14 +3,43 @@ package ie.atu.songmanager;
 public class Song {
 
     // Instance Variables 
-    private String songName; // Unique ID
-    private int numberOfPlays; 
-    private double Length;
-    private boolean isSold;
+    private int idNumber; // Unique ID
+    private String name; 
+    private float numberOfPlays; 
+    private double length;
+
 
     // Getter and Setter Methods
+    public int getidNumber(){
+        return this.idNumber;
+    }
 
-    
+    public String name(){
+        return this.name;
+    }
+
+    public float numberOfPlays(){
+        return this.numberOfPlays;
+    }
+
+    public double length(){
+        return this.length;
+    }
+
+    public void setidNumber(int idNumber){
+        this.idNumber = idNumber;
+    }
+    public void setname(String name){
+        this.name = name;
+    }
+    public void setnumberOfPlays(float numberOfPlays){
+        this.numberOfPlays = numberOfPlays;
+    }
+    public void setlength(double Lenght){
+        this.numberOfPlays = numberOfPlays;
+    }
+
+
 
 
 }
